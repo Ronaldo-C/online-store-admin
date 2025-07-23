c0936f4a665cde293183c0b0d178c743a79645b4
HuiWu <ronaldo_w@qq.com>
2025-07-23 11:35:17 +0800

    Revert "fix: deploy"
    This reverts commit 7319e267b6c37828ea7a5bc50a0ffc1b3b54fb7e.

--- vite.config.ts
+++ vite.config.ts
@@ -5,9 +5,6 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    assetsDir: 'admin',
-  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
