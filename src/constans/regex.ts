01d026bb77698dec4e8f4e8de83cd7853b0855c3
HuiWu <ronaldo_w@qq.com>
2025-07-27 12:05:26 +0800

    fix: bugs

--- src/constans/regex.ts
+++ src/constans/regex.ts
@@ -2,4 +2,5 @@ export const EMAIL_REGEX = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/
 
 export const USER_NAME_REGEX = /^[A-Za-z0-9_,.]{4,20}$/
 
-export const isValidUrl = /^https?:\/\/([\da-z.-]+)\.([a-z.]{2,6})([/\w .-]*)*\/?$/
+export const isValidUrl =
+  /^https?:\/\/(([\\da-z.-]+)\.([a-z.]{2,6})|((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)(\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}))([/\w .-]*)*\/?$/
